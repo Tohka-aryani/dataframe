63c0992263b763ac110a3127c7eaecdb0aadfa06
Tohka-aryani <129815399+Tohka-aryani@users.noreply.github.com>
2023-05-15 13:37:19 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 # Load data into a pandas DataFrame
-data = pd.read_csv("prn14_result_dun.csv")
+data = pd.read_csv("politiko 2/prn14_result_dun.csv")
 
 # Group data by state and abbreviation, and calculate the sum of wins for each group
 totals = data.groupby(["NEGERI", "PARTI"])["STATUS"].sum().reset_index()
